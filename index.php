cfc9cc25ccdb6c40c1e50a9ee551a99f72501bfa
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-13 07:59:51 +0200

    basic css added 13.10.23 07:59

--- index.php
+++ index.php
@@ -13,6 +13,14 @@
 
     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get">
         <input type="number" name="num01" placeholder="Number one">
+        <select name="operator">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="num02" placeholder="Number two">
+        <button>Calculate</button>
     </form>
 
 </body>
