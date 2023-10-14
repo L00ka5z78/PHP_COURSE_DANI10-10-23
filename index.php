1bbe2677ca2b2fdc5161dd11595dea646774c865
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-14 10:42:54 +0200

    insert method added 14.10.23 10:42

--- index.php
+++ index.php
@@ -15,105 +15,14 @@ declare(strict_types=1);
 </head>
 
 <body>
+    <h2>Signup</h2>
+    <form action="includes/formhandler.inc.php" method="post">
+        <input type="text" name="username" placeholder="username...">
+        <input type="password" name="pwd" placeholder="password...">
+        <input type="email" name="email" placeholder="email...">
 
-    <?php
-    // $test = "danel";
-
-    // function myFunction($test)
-    // {
-    //     //def local var
-    //     $localVar = "hello world";
-    //     //use local var
-    //     return $test; //output hello world
-    // }
-    // echo myFunction($test);
-
-    // $test = "danel";
-
-    // function myFunction($test)
-    // {
-    //     global $test;
-    //     //def local var
-    //     $localVar = "hello world";
-    //     //use local var
-    //     return $test; //output hello world
-    // }
-    // echo myFunction($test);
-
-    // $test = "danel";
-
-    // function myFunction()
-    // {
-    //     global $test;
-    //     //def local var
-    //     $localVar = "hello world";
-    //     //use local var
-    //     return $GLOBALS["test"]; //output hello world
-    // }
-    // echo myFunction();
-    // define("PI", 3.14);
-    // echo PI;
-
-    //** ********************************   loops *********************************/
-
-    //for loop
-
-    // for ($i = 0; $i <= 10; $i++) {
-    //     echo "This is iteration number " . $i . "<br>";
-    // }
-
-    // $test = 5;
-    // for ($i = 0; $i <= $test; $i++) {
-    //     echo "This is iteration number " . $i . "<br>";
-    // }
-
-    // *********************** while ************************************
-
-    // $boolean = true;
-    // while ($boolean) {
-    //     echo $boolean;
-    //     $boolean = false;
-    // }
-
-    // $test = 5;
-    // $boolean = true;
-    // while ($test < 10) {
-    //     echo $test;
-    //     $test++;
-    // }
-
-    // ********************************* do while *****************************
-
-    // $test = 10;
-    // do {
-    //     echo $test;
-    //     $test++;
-    // } while ($test < 10);
-
-    // ****************** for each ***********************************
-
-    // $fruits = array("apple", "banana", "orange");
-
-    // foreach ($fruits as $fruit) {
-    //     echo "this is: " . $fruit . "<br>";
-    // }
-
-    // displays only colors == keys *****************************
-
-    // $fruits = array("apple" => "RED", "banana" => "YELLOW", "orange" => "ORNGE");
-
-    // foreach ($fruits as $fruit) {
-    //     echo "this is: " . $fruit . "<br>";
-    // }
-
-    // displays both: values and keys
-    $fruits = array("apple" => "RED", "banana" => "YELLOW", "orange" => "ORNGE");
-
-    foreach ($fruits as $fruit => $color) {
-        echo "this is: " . $fruit . ", that hac color of " . $color . "<br>";
-    }
-    ?>
-
+        <button type="submit">Signup</button>
+    </form>
 </body>
 
 </html>
