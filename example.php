4628b8443bd2d4e7f482b9e8d81536f496f0ca46
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-14 18:23:57 +0200

    php cookie sessions

--- example.php
+++ example.php
@@ -1,12 +1,6 @@
 <?php
-// session_start();
-// $_SESSION["username"] = "Krossing";
-// session_unset(); //unset all session data
-// unset($_SESSION["username"]); //unset one session data
-// session_destroy();
-//unsets session ID on the server, not session id cookie in the browser
+session_start();
 
-require_once 'config.php';
 ?>
 
 
@@ -22,7 +16,6 @@ require_once 'config.php';
 </head>
 
 <body>
-
     <?php
     echo $_SESSION["username"];
     ?>
