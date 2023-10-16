0734b15c9e90b5ca296592d9e1d187ede1ab58ce
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-16 08:21:12 +0200

    signup.inc added 16.10.23 08:21

---
+++ includes/signup.inc.php
@@ -0,0 +1,17 @@
+<?php
+// check if user access page in right way
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $username = $_POST["username"];
+    $pwd = $_POST["pwd"];
+    $email = $_POST["email"];
+
+    try {
+        require_once 'dbh.inc.php';
+    } catch (PDOException $e) {
+        die("Query failed: " . $e->getMessage());
+    }
+} else {
+    header("Loaction: ../index.php");
+    die();
+}
