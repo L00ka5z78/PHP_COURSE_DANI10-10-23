154100799d3a3e5f35b3558a8a9db3b1dbc965c4
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-16 08:08:16 +0200

    check if session exist insie website 16.10.23 08:08

--- includes/config_session.inc.php
+++ includes/config_session.inc.php
@@ -5,11 +5,27 @@ ini_set('session.use_strict_mode', 1);
 
 session_set_cookie_params(
     [
-        'lifetime' => 1800,
+        'lifetime' => 1800, //30min
         'domain' => 'localhost',
         'path' => '/',
         'secure' => true,
         'httponly' => true
     ]
-
 );
+session_start();
+//check if session exist inside website
+
+if (!isset($_SESSION["last_regeneration"])) {
+    regenrate_session_id();
+} else {
+    $interval = 60 * 30;
+    if (time() - $_SESSION["last_regeneration"] >= $interval) {
+        regenrate_session_id();
+    }
+}
+
+function regenrate_session_id()
+{
+    session_regenerate_id();
+    $_SESSION["last_regeneration"] = time();
+}
