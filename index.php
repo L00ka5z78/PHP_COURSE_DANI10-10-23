ccf17ac78e9ab280ce5448ddca8f1f0d4e12829d
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-10-17 08:03:10 +0200

    first commit 17.10.23 08:03

--- index.php
+++ index.php
@@ -17,48 +17,21 @@ require_once 'includes/login_mvc/login_view.inc.php';
 </head>
 
 <body>
-    <h3>
-        <?php
-        output_username();
-        ?>
-    </h3>
 
     <?php
-    if (!isset($_SESSION["user_id"])) { ?>
-        <h2>Login</h2>
-        <form action="includes/login.inc.php" method="post">
-            <input type="text" name="username" placeholder="username...">
-            <input type="password" name="pwd" placeholder="password...">
+    $brand = "volvo";
+    $color = "green";
 
-            <button type="submit">Login</button>
-        </form>
+    function getCarInfo($brand, $color)
+    {
+        return "BRand: " . $brand . ", Color: " . $color;
+    }
 
-    <?php } ?>
-
-    <?php
-    check_login_errors();
+    echo getCarInfo($brand, $color);
 
     ?>
 
 
-    <h2>Signup</h2>
-    <form action="includes/signup.inc.php" method="post">
-
-        <?php
-        signup_inputs();
-        ?>
-
-        <button type="submit">Signup</button>
-    </form>
-
-    <?php
-    check_signup_errors();
-    ?>
-
-    <h2>Logout</h2>
-    <form action="includes/logout.inc.php" method="post">
-        <button type="submit">Logout</button>
-    </form>
 
 </body>
 
